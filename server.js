97af273073858bab5a917c04d0750d1eea0f7d58
youngcoder221 <laminendong087@gmail.com>
2026-07-01 15:00:03 +0000

    Configuration du CORS sur server.js avec FRONT_URL

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
 app.use(express.json());
 
 // Routes
