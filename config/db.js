12f3674e8dc22019aef17e2a932604e539f59079
youngcoder221 <laminendong087@gmail.com>
2026-07-01 14:10:40 +0000

    Ajout DB_PORT pour Railway

--- config/db.js
+++ config/db.js
@@ -2,8 +2,9 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host:     process.env.DB_HOST,
-  user:     process.env.DB_USER,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
